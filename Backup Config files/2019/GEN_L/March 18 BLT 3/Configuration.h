98a8fc7d327b4d70223a57927413af35c19d8648
RudyBenner <benner@vianet.ca>
2019-03-26 09:45:45 -0400

    Update

--- Backup Config files/2019/GEN_L/March 18 BLT 3/Configuration.h
+++ Backup Config files/2019/GEN_L/March 18 BLT 3/Configuration.h
@@ -690,7 +690,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 200, 200, 800, 241 } //{ 80, 80, 4000, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 200, 200, 800, 241 }
 
 /**
  * Default Max Feed Rate (mm/s)
